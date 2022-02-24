4b2bb2bf3503101534f6d2d9e55ec6a9b982d255
Carlos Carvalho <jcarloss.carvalho@gmail.com>
2022-02-24 20:31:30 -0300

    set headers to remove cache on get cover

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.get('/url', (req, res) => res.redirect(getLastSong().uri));
-app.get('/cover', (req, res) => res.sendFile('cover.png', { root: __dirname }));
+app.get('/cover', (req, res) => {
+  res.header('Pragma', 'no-cache');
+  res.header('Content-Type', 'image/png');
+  res.header('Expires', '0');
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.sendFile('cover.png', { root: __dirname })
+});
 
 app.listen(process.env.PORT || 3000, () => {
   getActualSongJob.invoke();
